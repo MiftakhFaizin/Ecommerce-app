af42acc0ba37942c512ee00a419368424fa65d6b
MiftakhFaizin <miftakhdamar07@gmail.com>
2025-01-12 20:49:17 +0700

    add alert in login page

--- src/Auth/LoginPage.jsx
+++ src/Auth/LoginPage.jsx
@@ -10,12 +10,15 @@ const LoginPage = () => {
     const dispatch = useDispatch()
     const navigate = useNavigate()
     const [loading, setLoading] = useState(false)
+    const [error, setError] = useState("")
 
     const handleUsernameInput = (e) => {
+        setError("")
         setUsernameInputValue(e.target.value)
     }
 
     const handlePasswordInput = (e) => {
+        setError("")
         setPasswordInputValue(e.target.value)
     }
 
@@ -30,31 +33,42 @@ const LoginPage = () => {
                 if(findUser) {
                     if(findUser.password === passwordInputValue) {
                         dispatch(login(findUser.id))
+                        setLoading(false)
                         navigate("/")
                     } else {
-                        throw "Wrong password"
+                        throw "Invalid password"
                     }
                 } else {
-                    throw "Wrong username"
+                    throw "Invalid username"
                 }
             } catch(err) {
-                alert(err)
-            } finally {
                 setLoading(false)
+                setError(err)
             }
         }
-        fetchDataUsers()
+
+        if(usernameInputValue && !passwordInputValue) {
+            setError("Please enter your password")
+        } else if(!usernameInputValue && passwordInputValue) {
+            setError("Please enter your username")
+        } else if(!usernameInputValue && !passwordInputValue) {
+            setError("Please enter your username and password")
+        } else {
+            fetchDataUsers()
+        }
     } 
 
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-200">
-            <div className="flex flex-col justify-around bg-white border border-slate-300 rounded-md shadow-md shadow-slate-400 py-[20px] px-[30px]">
-                <p className="font-sans font-bold text-[30px]">Welcome Back!</p>
-                <div className="flex flex-col gap-[25px] py-[40px]">
+        <div className="flex justify-center items-center h-screen bg-gradient-to-br from-gray-200 to-gray-400">
+            <div className="flex flex-col justify-around w-[400px] h-[400px] bg-white border border-slate-300 rounded-lg shadow-md shadow-slate-400 py-[20px] px-[30px]">
+                <p className="font-sans font-bold text-[30px] text-center">Welcome Back!</p>
+                <p className="font-sans text-[15px] text-center text-gray-600">Login to your account</p>
+                <div className="flex flex-col pt-[30px]">
                     <input onChange={handleUsernameInput} type="text" placeholder="username" className="border-b border-slate-500 outline-none font-sans text-[15px] pb-[10px]"/>
-                    <input onChange={handlePasswordInput} type="password" placeholder="password" className="border-b border-slate-500 outline-none font-sans text-[15px] pb-[10px]"/>
+                    <input onChange={handlePasswordInput} type="password" placeholder="password" className="border-b border-slate-500 outline-none font-sans text-[15px] mt-[25px] pb-[10px]"/>
+                    <p className={`text-red-600 text-[15px] h-[5px] mt-[10px] ${error ? 'visible' : 'invisible'}`}>{error}</p>
                 </div>
-                <button onClick={handleLoginBtn} className="bg-black rounded-md font-sans text-white font-bold py-[7px] px-[10px]">Login</button>
+                <button onClick={handleLoginBtn} className="bg-black rounded-md font-sans text-white font-bold mt-[10px] py-[7px] px-[10px]">Login</button>
             </div>
             {loading ?
             <SpinLoaderLogin />
--- src/Components/SpinLoader.jsx
+++ src/Components/SpinLoader.jsx
@@ -3,7 +3,10 @@ import loadingIcon from "../assets/load.png"
 export const SpinLoaderLogin = () => {
     return (
         <div className="flex justify-center items-center fixed bg-black bg-opacity-50 h-screen top-0 right-0 bottom-0 left-0 z-2">
-           <img className="object-contain w-[60px] h-[60px] animate-spin" src={loadingIcon}></img>
+            <div className="flex flex-col">
+                <img className="object-contain w-[60px] h-[60px] animate-spin" src={loadingIcon}></img>
+                <p className="text-white text-[18px] font-bold pl-[4px] mt-[10px] animate-pulse">Login...</p>
+            </div>
         </div>
     )
 }
